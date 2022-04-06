502cb1d5f1ca0cfcf23bc5de602bc78d47d27076
Md. Nahidul Islam <nahidulss@gmail.com>
2022-04-06 23:38:33 +0600

    two usm using hashing map

--- two sum/main.go
+++ two sum/main.go
@@ -42,10 +42,29 @@ func find_pair_sum2(nums []int, target int) (int, int) {
 	return -1, -1
 }
 
+//map hashing
+func find_pair_sum3(nums []int, target int) (int, int) {
+	mp := make(map[int]int)//empty map
+	//if(mp[nums] )
+
+	for i := 0; i < len(nums)-1; i++ {
+		if mp[nums[i]] == target-nums[i] {
+			return mp[nums[i]], nums[i]
+		} else {
+			mp[target-nums[i]] = nums[i]
+		}
+
+	}
+
+	return -1, -1
+}
+
 func main() {
 	nums := []int{4, 3, 2, 5, 1, 10, 7, 100, 23, 27}
-	target := 9
+	target := 27
+
 	//p1,p2 := find_pair_sum1(nums, target)
-	p1, p2 := find_pair_sum2(nums, target)
+	//p1, p2 := find_pair_sum2(nums, target)
+	p1, p2 := find_pair_sum3(nums, target)
 	fmt.Println(p1, p2)
 }
